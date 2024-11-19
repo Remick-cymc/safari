71897700fb6bbce74186267a888ab6ed39597f58
Remickcymc <tukeiremick@gmail.com>
2024-11-19 12:10:12 +0300

    myfirstapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-info'>
+      <h1 className='text-center'>Safari Rally Kenya</h1>
+      <p className='bg-danger'>The safari Rally Kenya is part of the world Rally Championship (WRC), placing it among the most prestigious rally event globally.
+        Drivers face extrem challenges as they navigate through unpredictrable weather and tough off-road conditions
+      </p>
+      <p className='bg-danger'>This event showcases Kenya's commitment to promoting sports tourism, drawing fans from across the globe to witness the 
+        adrenaline-fueled competition against the backdrop of scenic savannahs
+      </p>
+      <button type='button' className='btn btn-secondary'>EXPLORE WRC</button>
+      <br />
+      <p className='bg-warning'>The Safari Rally offers unique blend of motosport and wildlife conservation.Many stages of the rally pass through iconic game
+        reserves, offering glimpses of Kenyan's incredible wildlife, including lions, elephants, and girrafes.
+        Rally enthusiasts also enjoy eco-tourism activities, making the safari Rally a perfect combination of speed and sustainableity
+      </p>
+      <br />
+      <button type='button' className='btn btn-dark'>DISCOVER MORE</button>
+      <br />
+      <p className='bg-secondary'>Safari RAlly Kenya is not only about speed but also strategy and survival. The unforgiving terrains challenge drivers and vehicles, 
+        demandind precision, enduarance, teamwork to conquet
+        This makes it one of the most unpredictable rallies where even the smallest mistake can have a significant implact on the outcome
+      </p>
+      <br />
+      <button type='button' className='btn btn-warning'>LEARN STRATEGIES</button>
+      <br />
+      <p className='bg-primary'>The safari Rally has a rich history of legends, with iconic rally drivers leaving their mark on this incredible event. From the early
+        days of classic cars to modern rally machines, the event continues to evolve.
+        Fans celebrate the rally not only for its action- packed races but also for its nostalgic connections to Kenya's sporting heritage.
+      </p>
+      <br />
+      <button type='button' className='btn btn-danger'>CELEBRATE HISTORY</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
